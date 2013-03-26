6faf2d1e3a11d7f5702f6233cfa10ba3419df01e
kean wei ho <sensorbrain@gmail.com>
2013-03-26 10:21:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkfoodNetwork::Application.config.secret_token = 'df90b0a098178324f58746ce397c31b2986a8a823ac15f059e88a90883343563b4002859c4b63038c8856a704cd93a93a741ea204e35096569b0bbc4faa73065'
